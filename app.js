4952525e12164f010079375302f7f3bdf481a0f0
MattB <104602457+mbencomo2@users.noreply.github.com>
2023-04-26 12:50:21 -0700

    setup mongoDB connection

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express  = require('express');
+const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Fei Fong Wong');
+app.get("/", (req, res) => {
+  res.send("Fei Fong Wong");
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+const mongodb = require('./connections/mongodb.cjs');
+mongodb.run();
---
+++ connections/mongodb.cjs
@@ -0,0 +1,40 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@atlascluster.dysclee.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+    await listDatabases(client);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => {
+    console.log(` - ${db.name}`);
+  });
+}
+
+module.exports = { run };
