df10fe5bda714c2e623f3421e7921880b82ab697
MattB <104602457+mbencomo2@users.noreply.github.com>
2023-04-27 21:11:16 -0700

    -Set access control header

--- controller/userController.js
+++ controller/userController.js
@@ -4,6 +4,7 @@ const getUser = async (req, res, next) => {
   const result = await mongodb.getDb().db().collection("users").find();
   result.toArray().then((lists) => {
     res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.status(200).json(lists[0]);
   });
 };
@@ -12,6 +13,7 @@ const getUsername = async (req, res, next) => {
   const result = await mongodb.getDb().db().collection("users").find();
   result.toArray().then((lists) => {
     res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.status(200).json(lists[0].username);
   });
 };
