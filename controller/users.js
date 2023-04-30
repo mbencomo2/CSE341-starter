88b5e6787a029a0c30594e1bf003c2e5e5394b6f
MattB <104602457+mbencomo2@users.noreply.github.com>
2023-04-29 17:07:37 -0700

    - Users controller searches for users with a param

--- controller/users.js
+++ controller/users.js
@@ -1,21 +1,21 @@
 const mongodb = require("../db/connect");
 
-const getUser = async (req, res, next) => {
+const getUser = async (req, res) => {
+  const result = await mongodb.getDb().db().collection("users").find();
+  result.toArray().then((lists) => {
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).json(lists);
+  });
+};
+
+const getUsername = async (req, res) => {
   const result = await mongodb.getDb().db().collection("users").find();
   result.toArray().then((lists) => {
     const searchParam = req.params.id;
     res.setHeader("Content-Type", "application/json");
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.status(200).json(lists[searchParam]);
-  });
-};
-
-const getUsername = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection("users").find();
-  result.toArray().then((lists) => {
-    res.setHeader("Content-Type", "application/json");
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.status(200).json(lists[0].username);
+    res.status(200).json(lists[searchParam].username);
   });
 };
 
