106e0eddb748977254d532bb943d6fccd1b9a057
MattB <104602457+mbencomo2@users.noreply.github.com>
2023-05-10 11:37:32 -0700

    - Added swagger-autogent - Created new route for swagger ui - Updated .rest file for testing

--- controller/contacts.js
+++ controller/contacts.js
@@ -2,6 +2,7 @@ const mongodb = require("../db/connect");
 const ObjectId = require("mongodb").ObjectId;
 
 const getContacts = async (req, res) => {
+  // #swagger.summary = 'Grabs all contacts from the database'
   const result = await mongodb.getDb().db("test").collection("contacts").find();
   result.toArray().then((lists) => {
     res.setHeader("Content-Type", "application/json");
@@ -10,6 +11,7 @@ const getContacts = async (req, res) => {
 };
 
 const getContact = async (req, res) => {
+  // #swagger.summary = 'Grab a single contact by id'
   const searchParam = req.params.id;
   const o_id = new ObjectId(searchParam);
   const result = await mongodb.getDb().db("test").collection("contacts").find({ _id: o_id });
@@ -24,6 +26,19 @@ const getContact = async (req, res) => {
 };
 
 const createContact = async (req, res) => {
+/*    #swagger.parameters['obj'] = {
+        in: 'body',
+        description: 'Adding new contact.',
+        schema: {
+          $firstName: 'any',
+          $lastName: 'any',
+          $email: 'any',
+          $birthday: 'any',
+          $favColor: 'any'
+        }
+      } 
+      #swagger.summary = 'Create a new contact'
+*/
   try {
     const body = req.body;
     const result = await mongodb.getDb().db("test").collection("contacts").insertOne(body);
@@ -35,6 +50,19 @@ const createContact = async (req, res) => {
 };
 
 const updateContact = async (req, res) => {
+  /*    #swagger.parameters['obj'] = {
+          in: 'body',
+          description: 'Updating a contact.',
+          schema: {
+            $firstName: 'any',
+            $lastName: 'any',
+            $email: 'any',
+            $birthday: 'any',
+            $favColor: 'any'
+          }
+        }
+        #swagger.summary = 'Make a change to an existing contact. This also creates a new contact if one does not exist.'
+  */
   try {
     const id = new ObjectId(req.params.id);
     const body = req.body;
@@ -57,6 +85,7 @@ const updateContact = async (req, res) => {
 };
 
 const deleteContact = async (req, res) => {
+  // #swagger.summary = 'Delete a contact. Requires the contact's id'
   try {
     const id = new ObjectId(req.params.id);
     const result = await mongodb.getDb().db("test").collection("contacts").deleteOne({ _id: id });
